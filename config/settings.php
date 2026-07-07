f0e3a74da03a537341b51fa4cc522daf3cf8eeaa
tlsseptember2025-cloud <tls.september.2025@gmail.com>
2026-07-07 16:48:25 +0400

    new 20-5-26

--- config/settings.php
+++ config/settings.php
@@ -6,4 +6,6 @@ require_once __DIR__ . '/company.php';
 define('ZOOM_LINK_HOUR', 'https://us05web.zoom.us/j/89973469959?pwd=0YhZQ6mG3NUeCutnriiS90orDqxdQC.1');
 define('ZOOM_LINK_HALF', 'https://us05web.zoom.us/j/88580409983?pwd=fJZKnMgKCZwN7eZJs5n0Zy5i6L6vnb.1');
 
+define('UPLOAD_URL', '/uploads/');
+
 require_once __DIR__ . '/workflow.php';
