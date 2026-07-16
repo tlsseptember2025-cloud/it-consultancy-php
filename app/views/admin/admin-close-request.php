bd1278f216229b36aa5d97e21daa67229524bf29
tlsseptember2025-cloud <tls.september.2025@gmail.com>
2026-07-16 14:36:35 +0400

    new 20-5-26

---
+++ app/views/admin/admin-close-request.php
@@ -0,0 +1,11 @@
+<?php require VIEW_PATH.'/layouts/header-admin.php'; ?>
+
+<div class="container py-5">
+
+    <h2>Reschedule Consultation</h2>
+
+    <p>Customer wants to close the request!</p>
+
+</div>
+
+<?php require VIEW_PATH.'/layouts/footer.php'; ?>
